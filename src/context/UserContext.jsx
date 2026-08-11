df3e48c6f31fb41a161cf32085881f06c1db137a
rajsirajsi51-11 <rajsirajsi51@gmail.com>
2026-08-11 14:08:31 +0530

    Delete src directory

--- src/context/UserContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-export const UserContext = createContext();
-export function UserProvider({ children, user }) {
-  return (
-    <UserContext.Provider value={user}>
-      {children}
-    </UserContext.Provider>
-  );
-}
